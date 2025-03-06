9bba9b7131de18ffdba83bbeac0a13e39acd7e4c
Deyvid0104 <159158572+Deyvid0104@users.noreply.github.com>
2025-03-06 23:57:23 +0100

    Add frontend/server.py 1741301844338

---
+++ frontend/server.py
@@ -0,0 +1,18 @@
+import http.server
+import socketserver
+
+PORT = 8000
+DIRECTORY = "out"
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        super().end_headers()
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving at port {PORT}")
+    print(f"Open http://localhost:{PORT} in your browser")
+    httpd.serve_forever()
